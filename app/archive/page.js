db347cedbe4ebc37e276bb65c47b19afc69f9933
Ali-Jaffery786 <alimjaffery5@gmail.com>
2024-04-16 17:48:57 -0700

    Added Sanity.io

--- app/archive/page.js
+++ app/archive/page.js
@@ -1,42 +1,90 @@
 export default function ArchivePage() {
+
     return (
-        <div>
-            <header className="text-center font-bold p-10 pb-5 text-2xl">
+        <div className="bg-black">
+            <header className="text-center font-bold p-10 pb-5 text-2xl text-white">
                 <h1>MESH ARCHIVE</h1>
             </header>
-            <div className="text-center mb-10">
+            <div className="text-center mb-10 text-white">
                 <p1>View Photo Albums of past MESH events</p1>
             </div>
             <div className="flex flex-row-reverse mr-20 ">
                 <button class="bg-slate-300 rounded-lg w-24">sort by &#8595;</button>
             </div>
-            <div className="grid grid-cols-3">
-                <Block />
-                <Block />
-                <Block />
-                <Block />
-                <Block />
-                <Block />
-
+            <div>
+                <ArchiveBlock/>
             </div>
         </div>
     );
-
- function Block() {
-    return (
-        <div>
-            <div className="p-10">
-                <img src="tempImage.jpg" width="300" height="300" alt="When images are added, this should have real alt text" />
-            </div>
-            <div className="ml-14 inline-block">
-                <p>Title / Date</p>
-            </div>
-            <div className="ml-5 inline-block">
-                <button class="bg-slate-300 rounded-full px-5">view program</button>
-            </div>
-        </div>
-    );
- }
 
 
 }
+function ArchiveBlock() {
+    const archivesInfo = [
+        {
+            title: "The scene",
+            date: "2/24/2024",
+            picture: "/images/Arch1.JPG",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+        {
+            title: "Event Title",
+            date: "XX/XX/XXXX",
+            picture: "/images/Arch2.jpeg",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+        {
+            title: "Event Title",
+            date: "XX/XX/XXXX",
+            picture: "/images/Arch3.png",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+        {
+            title: "Event Title",
+            date: "XX/XX/XXXX",
+            picture: "/images/Arch4.jpeg",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+        {
+            title: "Event Title",
+            date: "XX/XX/XXXX",
+            picture: "/images/Arch5.jpeg",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+        {
+            title: "Event Title",
+            date: "XX/XX/XXXX",
+            picture: "/images/Arch6.jpeg",
+            alt: "tbd",
+            program: "links tbd",
+            
+        },
+    ];
+    return (
+        <ul className="grid grid-cols-3">
+        {archivesInfo.map((archive) => (
+            <div >
+                <div className="p-10">
+                    <img src={archive.picture} width="300" height="300" alt="When images are added, this should have real alt text" />
+                </div>
+                <div className="ml-14 inline-block text-white">
+                    <p >{archive.date} {archive.title}</p>
+                </div>
+                <div className="ml-5 inline-block">
+                    <button class=" rounded-full px-5 text-white">view program</button>
+                </div>
+            </div>   
+        ))}
+        </ul>
+    );
+ }
