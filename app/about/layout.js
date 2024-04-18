c0efd9de7b8e1c44351e1354cdc3537859d0f4d3
Ashika Palacharla <33813533+ashikapala@users.noreply.github.com>
2024-04-18 14:30:17 -0700

    about title + who are we

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,4 +1,19 @@
 import Navbar from "../components/Navbar";
+import { Jost } from "next/font/google";
+import "../globals.css";
+import localFont from "next/font/local"
+import Footer from "../components/Footer";
+
+const fabulous = localFont({
+  src: '../static-fonts/fabulous.otf',
+  display: 'swap',
+})
+
+const jost = Jost({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
 
 export default function Layout({ children }) {
   return <>{children}</>;
