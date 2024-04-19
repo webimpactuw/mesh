c9c3f6d93066c62a1b83f712b3772a74a3d497b1
jasminepw <115040328+jasminepw@users.noreply.github.com>
2024-04-18 19:48:06 -0700

    Jam session

--- app/page.js
+++ app/page.js
@@ -10,26 +10,42 @@ const fabulous = localFont({
 export default function Home() {
   return (
     <div>
-      <div>
-        image image image
-        <div className="mt-5 text-white text-center text-xl">
+      <div className="pt-4">
+        <div className="flex flex-row justify-between">
+          <img src="home_left.png" className="h-96" alt="Mesh fashion show"></img>
+          <img src="home_mid.png" className="h-96" alt="Mesh representatives"></img>
+          <img src="home_right.png" className="h-96"></img>
+        </div>
+        <div className="mt-5 text-white text-center text-xl mb-5">
         A collective of fashion designers & enthusiasts based at UW
         </div>
       </div>
       
 
-      <div className="latest-events content-center items-center">
+      <div className="flex flex-col justify-center latest-events content-center items-center m">
         <div className={fabulous.className}>
-          <h1 className="text-center text-white text-3xl">Latest Events</h1>
+          <h1 className="text-center text-white text-3xl m-4">Upcoming Events</h1>
         </div>
-        <div>
-          Image
-          Image
-          Image
+        <div className="flex flex-row justify-center mb-8n space-x-2 mx-8">
+          <div className="">
+            <div className="line"></div>
+            <img src="latest_left.png" alt="Mesh event" className="border border-solid border-white"></img>
+            <p className="text-white text-center font-light">text</p>
+          </div>
+          <div className="">
+            <div className="line"></div>
+            <img src="latest_mid.png" alt="Mesh event" className="border border-solid border-white"></img>
+            <p className="text-white text-center font-light">text</p>
+          </div>
+          <div className="">
+            <div className="line"></div>
+            <img src="latest_right.png" alt="Mesh event" className="border border-solid border-white"></img>
+            <p className="text-white text-center font-light">text</p>
+          </div>
         </div>
       </div>
 
-      <div>
+      <div className="mb-40">
         <div className={fabulous.className}>
           <h1 className="text-center text-white text-3xl">Instagram Feed</h1>
         </div>
