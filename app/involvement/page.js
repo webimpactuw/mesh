4b4a3a361d93838ba9b27d94a5880833316904cc
Cleo <121153199+cleoreyes@users.noreply.github.com>
2024-05-02 17:47:00 -0700

    changes

--- app/involvement/page.js
+++ app/involvement/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
       <div>
-        get involved
+        <header className="text-center font-bold p-10 pb-5 text-4xl">
+                <h1>Get Involved</h1>
+          </header>
+          <div className="text-center">
+            Sign up to receive updates on our open positions!
+          </div>
       </div>
     );
   }
