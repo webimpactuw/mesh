79b8ffa65c95bdcd889aebdd6823a38398c06373
Ruslana Korolov <rkorolov@icloud.com>
2024-05-20 21:56:50 -0700

    fixed fonts

--- app/events/layout.js
+++ app/events/layout.js
@@ -1,5 +1,3 @@
-import Navbar from "../components/Navbar";
-
 export default function Layout({ children }) {
-  return <><body class="bg-[#121212]">{children}</body></>;
+  return <>{children}</>;
 }
--- app/events/page.js
+++ app/events/page.js
@@ -5,20 +5,37 @@ import Image from 'next/image';
 import LearnButton from './components/LearnButton';
 import Events from './components/Events';
 import Navbar from '../components/Navbar';
+import { Jost } from "next/font/google";
+
+
+
+import "../globals.css";
+import localFont from "next/font/local"
+
+const fabulous = localFont({
+  src: '../static-fonts/fabulous.otf',
+  display: 'swap',
+})
+
+const jost = Jost({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '600'], 
+})
 
 
 import EventsMap from './components/EventMap';
 
 export default function events() {
-
-  
-  
   return (
     <div class="w-full h-full bg-[#121212]">
-       <Navbar/>
+       {/* <Navbar/> */}
       <div class="m-auto text-center">
-        <h1 class="text-4xl pb-6 text-[#FFFFFF]">MESH EVENTS</h1>
-        
+
+        <div className={fabulous.className}>
+        <h1 class="text-4xl pb-6 text-[#FFFFFF]">EVENTS</h1>
+        </div>
+
 
         <figure class="w-2/3 overflow-hidden m-auto b-[#FFFFFF] border-2">
           
@@ -27,10 +44,11 @@ export default function events() {
 
 
 
-        <div class="flex flex-row m-auto"></div> 
-        <p class="text-[#FFFFFF] w-3/5 py-4 m-auto">A yearly event where students, designers, and fashion enthusiasts come  together to explore style, creativity, and the limitless horizons of the fashion world</p>
+        <div className={jost.className}>
+        <p class="text-[#FFFFFF] w-3/5 py-4 m-auto text-xl font-light">A yearly event where students, designers, and fashion enthusiasts come  together to explore style, creativity, and the limitless horizons of the fashion world</p>
         <LearnButton />
-        
+        </div>
+
       </div>
 
       <div class="flex flex-col ml-28">
