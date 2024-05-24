cc942cdfbcb068c0505650cad9977b15542fbde2
Ruslana Korolov <rkorolov@icloud.com>
2024-05-23 17:15:03 -0700

    Update schema.js

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,6 +2,7 @@ import positions from "./schema/documents/positions.js"
 import archive from "./schema/documents/archive.js"
 import members from "./schema/documents/members.js"
 import events from "./schema/documents/events.js"
+import eventPage from "./schema/documents/eventPage.js"
 export const schema = {
-  types: [archive, members, events, positions]
+  types: [archive, members, events, positions, eventPage]
 }
